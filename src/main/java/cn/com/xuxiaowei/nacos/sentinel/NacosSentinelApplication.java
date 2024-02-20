2ef109f0923b0fc89165dfcab4061c4360ecc299
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-02-20 13:31:49 +0800

    init

---
+++ src/main/java/cn/com/xuxiaowei/nacos/sentinel/NacosSentinelApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.nacos.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosSentinelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosSentinelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/com/xuxiaowei/nacos/sentinel/NacosSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.nacos.sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosSentinelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
