d7fd4457d21f8ac8bae00b9b62db5ca59e85a89c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-02-22 11:31:39 +0800

    :sparkles: 飞书 Webhook：spring-javaformat:apply

--- src/main/java/cn/com/xuxiaowei/nacos/sentinel/webhook/WebHookWeixin.java
+++ src/main/java/cn/com/xuxiaowei/nacos/sentinel/webhook/WebHookWeixin.java
@@ -4,6 +4,6 @@ package cn.com.xuxiaowei.nacos.sentinel.webhook;
  * @author xuxiaowei
  * @since 0.0.1
  */
-public interface WebHookWeixin extends WebHook{
+public interface WebHookWeixin extends WebHook {
 
 }
